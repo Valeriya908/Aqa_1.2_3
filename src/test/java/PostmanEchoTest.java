755693f2b735c3cacba28720f81d8cb36edadc51
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-12-14 13:34:16 +0300

    Initial Commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some argument")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some argument"));
+    }
+}
