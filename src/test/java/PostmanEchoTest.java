459570e563aad4879b573b7a962df06c991f9ab1
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-12-14 13:43:54 +0300

    final version (corrected test)

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,6 +14,6 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some text"));
+                .body("data", equalTo("some argument"));
     }
 }
